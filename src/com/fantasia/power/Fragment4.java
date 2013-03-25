fa0e5710fb55e010f27117c7e6e94857a2e0cfcd
wenlong-wu <wenlong.wu@oceanwing.com>
2013-03-25 19:01:42 +0800

    re

---
+++ src/com/fantasia/power/Fragment4.java
@@ -0,0 +1,23 @@
+package com.fantasia.power;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment4 extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.list, container, false);
+	}
+}
--- src/com/fantasia/power/MainActivity.java
+++ src/com/fantasia/power/MainActivity.java
@@ -1,22 +1,52 @@
 package com.fantasia.power;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	private TabHost mTabHost;
+	private TabManager mTabManager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+        mTabManager = new TabManager(this, mTabHost, R.id.realtabcontent);
+        
+        mTabHost.setCurrentTab(0);//設定一開始就跳到第一個分頁
+        mTabManager.addTab(
+                mTabHost.newTabSpec("Fragment1").setIndicator("Fragment1",
+                this.getResources().getDrawable(
+                    android.R.drawable.ic_dialog_alert)),Fragment1.class, null);
+            mTabManager.addTab(
+                mTabHost.newTabSpec("Fragment2").setIndicator("Fragment2",
+                this.getResources().getDrawable(
+                    android.R.drawable.ic_lock_lock)),Fragment2.class, null);
+            mTabManager.addTab(
+                mTabHost.newTabSpec("Fragment3").setIndicator("Fragment3",
+                this.getResources().getDrawable(
+                    android.R.drawable.ic_input_add)),Fragment3.class, null);
+            mTabManager.addTab(
+                mTabHost.newTabSpec("Fragment4").setIndicator("Fragment4",
+                this.getResources().getDrawable(
+                    android.R.drawable.ic_delete)),Fragment4.class, null);
+        
+        DisplayMetrics dm = new DisplayMetrics();   
+        getWindowManager().getDefaultDisplay().getMetrics(dm); //先取得螢幕解析度  
+        int screenWidth = dm.widthPixels;   //取得螢幕的寬
+           
+           
+        TabWidget tabWidget = mTabHost.getTabWidget();   //取得tab的物件
+        int count = tabWidget.getChildCount();   //取得tab的分頁有幾個
+        if (count > 3) {   
+            for (int i = 0; i < count; i++) {   
+                tabWidget.getChildTabViewAt(i)
+                      .setMinimumWidth((screenWidth)/3);//設定每一個分頁最小的寬度   
+            }   
+        }
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
